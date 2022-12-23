08379b2faaa29bde970ba681d95e0375dcb929b7
samandargaibov <samandar.gaibov@gmail.com>
2022-12-23 16:22:46 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Git remote with VS!");
+
+            int i=1;
+            while (i<100)
+            {
+                if(i%3==0)
+                {
+                    Console.WriteLine(i);
+                }
+                i++;
+            }
+        }
+    }
+}
